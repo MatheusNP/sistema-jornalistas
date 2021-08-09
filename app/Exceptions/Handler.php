4b18775495928fde5606b5715d3cc1450a2909cf
Matheus Nobre <matheusnobre56@gmail.com>
2021-08-09 09:21:36 -0300

    Improve in exception returns

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -21,6 +23,39 @@ class Handler extends ExceptionHandler
         HttpException::class,
         ModelNotFoundException::class,
         ValidationException::class,
+    ];
+
+
+    /**
+     * This mapping holds exceptions we're interested in and creates a simple configuration that can guide us
+     * with formatting how it is rendered.
+     *
+     * @var array|array[]
+     */
+    protected $exceptionMap = [
+        ModelNotFoundException::class => [
+            'code' => 404,
+            'message' => 'Could not find what you were looking for.',
+            'adaptMessage' => false,
+        ],
+
+        NotFoundHttpException::class => [
+            'code' => 404,
+            'message' => 'Could not find what you were looking for.',
+            'adaptMessage' => false,
+        ],
+
+        MethodNotAllowedHttpException::class => [
+            'code' => 405,
+            'message' => 'This method is not allowed for this endpoint.',
+            'adaptMessage' => false,
+        ],
+
+        \InvalidArgumentException::class => [
+            'code' => 400,
+            'message' => 'You provided some invalid input value',
+            'adaptMessage' => true,
+        ],
     ];
 
     /**
@@ -41,14 +76,52 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
-     *
-     * @throws \Throwable
+     * @param \Illuminate\Http\Request $request
+     * @param Throwable $exception
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response|\Symfony\Component\HttpFoundation\Response
+     * @throws Throwable
      */
     public function render($request, Throwable $exception)
     {
+
+        if ($response = $this->formatException($exception)) {
+
+            return response()->json(['error' => $response], $response['status'] ?? 500);
+        }
         return parent::render($request, $exception);
+    }
+
+
+    /**
+     * A simple implementation to help us format an exception before we render me
+     *
+     * @param \Throwable $exception
+     *
+     * @return array|null
+     */
+    protected function formatException(\Throwable $exception): ?array
+    {
+        # We get the class name for the exception that was raised
+        $exceptionClass = get_class($exception);
+
+        # we see if we have registered it in the mapping - if it isn't
+        # we create an initial structure as an 'Internal Server Error'
+        # note that this can always be revised at a later time
+        $definition = $this->exceptionMap[$exceptionClass] ?? null;
+
+        if ($definition) {
+            if (!empty($definition['adaptMessage'])) {
+
+                $definition['message'] = $exception->getMessage() ?? $definition['message'];
+
+            }
+
+            return [
+                'error_code' => $definition['code'] ?? 500,
+                'exception' => $definition['title'] ?? 'Error',
+                'message' => $definition['message'],
+            ];
+        }
+        return null;
     }
 }
